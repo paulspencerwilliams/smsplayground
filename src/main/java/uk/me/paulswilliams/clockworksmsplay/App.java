091baa53b3248e62ba653b0c62367c27df5e6832
Paul Williams <paul@paulswilliams.me.uk>
2014-10-18 13:36:54 +0100

    initial project creation

---
+++ src/main/java/uk/me/paulswilliams/clockworksmsplay/App.java
@@ -0,0 +1,13 @@
+package uk.me.paulswilliams.clockworksmsplay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

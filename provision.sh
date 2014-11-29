b0e74a8550631780b74d095a8f632fd91314e9f2
Paul Williams <paul@paulswilliams.me.uk>
2014-11-29 15:27:27 +0000

    updated provisioning

--- provision.sh
+++ provision.sh
@@ -1,9 +1,15 @@
 sudo apt-get update
 curl -L https://get.rvm.io | bash -s stable --ruby
 source /home/vagrant/.rvm/scripts/rvm
-sudo apt-get install postgresql
+sudo apt-get install postgresql -y
+sudo apt-get install libpq-dev -y
 cd /vagrant/server/
 bundle install
+sudo -u postgres   createuser -P -d -S smsplayground
+sudo service postgres restart
+ createdb -U smsplayground -W smsplayground
 sequel -m db/migrations/ postgres://smsplayground:itsasecret@localhost:5432/smsplayground
 rackup
 
+sudo apt-get install maven -y
+sudo apt-get install openjdk-7-jdk -y
